194740b19c60823b9ce3d4418a1c9cc83b7d98d9
Marcus Lease <inbox@marcuslease.digital>
2019-05-04 09:34:29 +0200

    changed response text

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const port = 3000;
 const helmet = require('helmet');
 app.use(helmet());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('som en blåhval brudt ned'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
